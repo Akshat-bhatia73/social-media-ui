3e2c9dbfa22bfba93e93dd8620d3b1211d646ca0
Akshat Bhatia <bakshat73@gmail.com>
2023-08-22 20:07:59 +0530

    Minor change

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,19 +1,20 @@
-import React from "react";
+import React, { useState } from "react";
 import LogoDark from "../../assets/LogoDark.svg";
+import Messenger from "../../assets/Messenger.png"
 import "./Navbar.scss";
 import {
   MdGridView,
-  MdHome,
-  MdPerson,
   MdSearch,
-  MdNotifications,
-  MdDarkMode,
+  MdOutlineHome,
+  MdOutlineNotifications,
+  MdOutlineDarkMode,
+  MdOutlinePerson,
 } from "react-icons/Md";
-import { FaFacebookMessenger } from "react-icons/fa";
 
 const Navbar = () => {
+
   return (
-    <div className="navbar">
+    <div className='navbar'>
       <div className="left">
         <img src={LogoDark} alt="" height={30} />
         <div className="search-box">
@@ -25,23 +26,23 @@ const Navbar = () => {
       </div>
       <div className="right">
         <div className="icon">
-          <MdHome size={30} />
+          <MdOutlineHome size={25} />
         </div>
         <div className="icon">
-          <FaFacebookMessenger size={20} />
+          <img src={Messenger} height={21} alt="" />
         </div>
         <div className="icon">
-          <MdNotifications size={30} stroke="1px" />
+          <MdOutlineNotifications size={30} stroke="1px" />
         </div>
         <div className="icon">
-          <MdDarkMode size={30} />
+          <MdOutlineDarkMode size={30} />
         </div>
         <div className="icon">
           <MdGridView size={30} />
         </div>
         <div className="profile">
           <div className="profile-image">
-            <MdPerson size={25} />
+            <MdOutlinePerson size={25} />
           </div>
           <h3 className="name">Akshat Bhatia</h3>
         </div>
