1f82b27a4b7b89288c3d833449f60690ddd7b42c
Akshat Bhatia <bakshat73@gmail.com>
2023-08-25 19:10:23 +0530

    Profile Page Ui completed

--- src/pages/Profile/Profile.jsx
+++ src/pages/Profile/Profile.jsx
@@ -1,7 +1,42 @@
+import { Posts } from "../../components";
+import './Profile.scss'
+import CoverPhoto from "../../assets/cover-photo.jpg"
+import ProfilePhoto from "../../assets/profile-photo.jpeg"
+import Messenger from "../../assets/Messenger.png"
+
 const Profile = () => {
   return (
-    <div>Profile</div>
-  )
-}
+    <div className="profile">
+      <div className="profile-card">
+        <div className="images">
+          <div className="blur"></div>
+          <img src={CoverPhoto} alt="" className="cover"/>
+          <img src={ProfilePhoto} alt="" className="profile-photo"/>
+        </div>
+        <div className="info">
+          <h3>Akshat Bhatia</h3>
+          <p>Developer, India</p>
+          <div className="stats">
+            <div className="followers">
+              <h4>197</h4>
+              <span>Followers</span>
+            </div>
+            <div className="following">
+              <h4>372</h4>
+              <span>Following</span>
+            </div>
+          </div>
+          <div className="actions">
+            <button>Follow</button>
+            <div className="icon">
+              <img src={Messenger} height={23} alt="" />
+            </div>
+          </div>
+        </div>
+      </div>
+      <Posts />
+    </div>
+  );
+};
 
-export default Profile
+export default Profile;
